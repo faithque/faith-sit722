a7fe4a44682f50097abca95b5a61f81e2219f828
faithque <wandabwafaith@gmail.com>
2025-09-24 06:50:53 +0300

    fix: point the db models to the correct db ports

--- backend/customer_service/app/db.py
+++ backend/customer_service/app/db.py
@@ -8,7 +8,7 @@ POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "customers")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5431")
 
 DATABASE_URL = (
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
